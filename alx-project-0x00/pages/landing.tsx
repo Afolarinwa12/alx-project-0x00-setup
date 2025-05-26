e5af17d991714af627074b510c1b12330fdf87fa
Afolarinwa12 <Afolarinwa12@gmail.com>
2025-05-26 23:10:51 +0100

    correction on task 6

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -7,9 +7,9 @@ const Landing: React.FC = ()  => {
              <Card />
               <div className="mb-6">
         <h2 className="text-xl font-semibold mb-2"></h2>
-        <Button title="Learn More" styles="text-sm rounded-sm bg-blue-500 text-white hover:bg-blue-600" />
-
-       
+         <Button title="Small Rounded-sm" styles="text-sm rounded-sm bg-blue-500 text-white hover:bg-blue-600" />
+        <Button title="Small Rounded-md" styles="text-sm rounded-md bg-green-500 text-white hover:bg-green-600" />
+        <Button title="Small Rounded-full" styles="text-sm rounded-full bg-red-500 text-white hover:bg-red-600" />
       </div>
         </div>
     )
